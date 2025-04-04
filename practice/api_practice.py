580e34ab2e7638d7938e0a2613d8235ab8490c02
misunderstood <2djohar@gmail.com>
2025-04-04 14:16:23 +0300

    api practice start

--- practice/api_practice.py
+++ practice/api_practice.py
@@ -1,5 +1,8 @@
 import requests
 
+urls = {
+    "https://api.coingecko.com/api/v3/":"coins/dogecoin",
+    "https://apis.scrimba.com/openweathermap/data/2.5/":"weather"}
 
 kiev_coords = (50.4501, 30.5234)
 base_url = "https://apis.scrimba.com/openweathermap/data/2.5/"
@@ -9,6 +12,8 @@ params = {
     "lon": kiev_coords[1],
 }
 
-res = requests.get(base_url + endpoint, params=params)
+# res = requests.get(base_url + endpoint, params=params)
+# res = requests.get("https://api.coingecko.com/api/v3/coins/dogecoin")
+res = requests.get("https://www.google.com/search?q=bed+sheets")
 print(res.status_code)
-print(res.json())
+print(res.content)
