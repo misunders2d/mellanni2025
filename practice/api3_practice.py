7f59252fdcf74477fccc4109bff85630e0dee49b
misunders2d <2djohar@gmail.com>
2025-04-18 14:55:18 +0300

    adding api3practice with openai library

---
+++ practice/api3_practice.py
@@ -0,0 +1,29 @@
+import openai
+import os
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+
+bot = openai.OpenAI(api_key=OPENAI_KEY)
+
+# text_res = bot.chat.completions.create(
+    
+#     model="gpt-4o-mini",
+#     messages=[
+#         {
+#             "role": "system",
+#             "content": "You are Grok, a highly intelligent, helpful AI assistant."
+#         },
+#         {
+#             "role": "user",
+#             "content": "What is the meaning of life, the universe, and everything?"
+#         },
+#     ],
+    
+# )
+
+img_res = bot.images.generate(
+    prompt="A futuristic city skyline at sunset, with flying cars and neon lights",
+    model="dall-e-3"  
+  
+)
+
+print(img_res.data[0].url)
