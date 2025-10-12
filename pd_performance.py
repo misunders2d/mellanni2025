72b59329538cd31f28ef42bfec9ef78406217e1d
misunderstood <2djohar@gmail.com>
2025-10-12 14:02:47 +0300

    updating event calculation (asin-grouping, adding all skus in one line)

--- pd_performance.py
+++ pd_performance.py
@@ -7,17 +7,23 @@ from common.events import event_dates, event_dates_list
 if __name__ == "__main__":
 
     d = Dataset(
-        start="2022-01-01", end="2025-12-31", market="US", local_data=True, save=False
+        start="2022-01-01", end="2026-12-31", market="US", local_data=False, save=False
     )
 
     d.pull_br_data()
+    d.pull_dictionary(marketplace="US")
     sales = d.br.copy()
+    dictionary = d.dictionary.copy()
+
+    dictionary_asin = (
+        dictionary[["asin", "sku"]].groupby("asin").agg(lambda x: ", ".join(x))
+    )
 
     sales["date"] = pd.to_datetime(sales["date"]).dt.date
     event_sales = sales[sales["date"].isin(event_dates_list)]
     non_event_sales = sales[~sales["date"].isin(event_dates_list)]
 
-    final = pd.DataFrame(columns=["sku"])
+    final = pd.DataFrame(columns=["asin"])
 
     for event, dates in event_dates.items():
         days = 60
@@ -28,7 +34,7 @@ if __name__ == "__main__":
             )
         ]
         sku_pre_event_average = (
-            pre_event_sales.groupby("sku")["unitsOrdered"].agg("sum").reset_index()
+            pre_event_sales.groupby("asin")["unitsOrdered"].agg("sum").reset_index()
         )
         sku_pre_event_average["unitsOrdered"] = (
             sku_pre_event_average["unitsOrdered"] / days
@@ -39,7 +45,9 @@ if __name__ == "__main__":
 
         specific_event_sales = event_sales[event_sales["date"].isin(dates)]
         sku_event_sales = (
-            specific_event_sales.groupby("sku")["unitsOrdered"].agg("sum").reset_index()
+            specific_event_sales.groupby("asin")["unitsOrdered"]
+            .agg("sum")
+            .reset_index()
         )
         sku_event_sales = sku_event_sales.rename(
             columns={"unitsOrdered": f"{event_name} total units"}
@@ -51,7 +59,7 @@ if __name__ == "__main__":
         )
 
         total_event_stats = pd.merge(
-            sku_pre_event_average, sku_event_sales, how="outer", on="sku"
+            sku_pre_event_average, sku_event_sales, how="outer", on="asin"
         )
 
         total_event_stats[f"{event_name} X increase"] = (
@@ -59,6 +67,10 @@ if __name__ == "__main__":
             / total_event_stats[f"pre-{event_name} avg units"]
         )
 
-        final = pd.merge(final, total_event_stats, how="outer", on="sku")
+        final = pd.merge(
+            final, total_event_stats, how="outer", on="asin", validate="1:1"
+        )
+
+    final = pd.merge(dictionary_asin, final, how="right", on="asin", validate="1:1")
 
     final.to_excel("/home/misunderstood/temp/event_stats.xlsx", index=False)
