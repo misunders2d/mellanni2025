781a127a81cf907f1c60a5a30d4871cca390278f
misunders2d <2djohar@gmail.com>
2025-12-19 13:23:25 +0200

    new price checker logic

--- scripts/price_checker.py
+++ scripts/price_checker.py
@@ -10,13 +10,13 @@ import customtkinter as ctk
 from utils import mellanni_modules as mm
 
 BQ_BUSINESS_REPORT: str = "reports.business_report"
-BQ_DICTIONARY: str = "auxillary_development.dictionary"
+# BQ_DICTIONARY: str = "auxillary_development.dictionary"
 BQ_ORDERS: str = "reports.all_orders"
 FBA_INVENTORY: str = "reports.fba_inventory_planning"
 SALE_FILE: str = "1iB1CmY_XdOVA4FvLMPeiEGEcxiVEH3Bgp4FJs1iNmQs"
-PRICELIST_ID: str = "1VGZ5VGsQiYgX9X6PxrRREj265gMzVQu9UQMKnT_014o"
+# PRICELIST_ID: str = "1VGZ5VGsQiYgX9X6PxrRREj265gMzVQu9UQMKnT_014o"
 EVENT_FILE: str = "1XcrMgklKRvElCb8vZI5r6j0P9ha4wmlPZdpU9MWq7QA"
-DICTIONARY_FILENAME: str = "Dictionary.xlsx"
+# DICTIONARY_FILENAME: str = "Dictionary.xlsx"
 DICTIONARY_FILE_ID: str = "1Y4XhSBCXqmEVHHOnugEpzZZ3NQ5ZRGOlp-AsTE0KmRE"
 marketplace: str = "US"
 
@@ -79,13 +79,22 @@ class App(ctk.CTk):
                 }
             )
         else:
-            fba_query = f"""SELECT DATE(snapshot_date) as snapshot_date, sku as SKU, available, your_price, sales_price
-                            FROM {FBA_INVENTORY}
-                            WHERE DATE(snapshot_date) = (
-                                SELECT MAX(DATE(snapshot_date)) FROM {FBA_INVENTORY} WHERE marketplace = "{marketplace}"
-                                )
-                            AND marketplace = "{marketplace}"
-                            """
+            fba_query = f"""
+            WITH RecentSnapshots AS (
+            SELECT
+                *,
+                ROW_NUMBER() OVER (PARTITION BY sku ORDER BY snapshot_date DESC) AS rn
+            FROM 
+                `mellanni-project-da.{FBA_INVENTORY}`
+            WHERE 
+                marketplace = "{marketplace}"
+                AND LOWER(condition) = "new"
+                AND snapshot_date >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 3 DAY)
+            )            
+            SELECT DATE(snapshot_date) as snapshot_date, sku as SKU, available, your_price, sales_price
+            FROM RecentSnapshots
+            WHERE rn=1
+            """
             fba_inv = self.client.query(fba_query).to_dataframe()
         self.result_files["fba_inventory"] = fba_inv
         self.print_area.insert(ctk.END, text="fba file downloaded\n")
@@ -103,7 +112,9 @@ class App(ctk.CTk):
         #     dm.download_file(DICTIONARY_FILE_ID),
         #     usecols=["SKU", "Collection", "Sub-collection", "Size Map", "Color"],
         # )
-        dictionary = dictionary_spreadsheet[["SKU", "Collection", "Sub-collection", "Size Map", "Color"]]
+        dictionary = dictionary_spreadsheet[
+            ["SKU", "Collection", "Sub-collection", "Size Map", "Color",'Standard price', 'MSRP']
+            ]
         dictionary = dictionary[~dictionary["Collection"].isin(excluded_collections)]
         del dictionary["Collection"]
         dictionary = dictionary.rename(
@@ -125,23 +136,11 @@ class App(ctk.CTk):
         self.print_area.insert(ctk.END, text="dictionary downloaded\n")
         return
 
-    def download_price_list(self):
-        price_list = dm.download_gspread(spreadsheet_id=PRICELIST_ID)[
-            ["Sub-collection", "Size", "Price", "MSRP"]
-        ]
-        price_list = price_list.rename(
-            columns={"Sub-collection": "collection", "Size": "size"}
-        )
-        self.result_files["price_list"] = price_list
-        self.print_area.insert(ctk.END, text="price list downloaded\n")
-        return
 
-    def merge_files(self, dictionary, price_list, fba_inventory, sale_file, event_file):
+    def merge_files(self, dictionary, fba_inventory, sale_file, event_file):
         self.print_area.insert(ctk.END, text="Merging files\n")
-        price_check = pd.merge(
-            dictionary, price_list, how="left", on=["collection", "size"]
-        )
-        price_check = pd.merge(price_check, fba_inventory, how="outer", on="SKU")
+        
+        price_check = pd.merge(dictionary, fba_inventory, how="outer", on="SKU")
         price_check = pd.merge(price_check, sale_file, how="outer", on="SKU")
         return price_check
 
@@ -208,7 +207,6 @@ class App(ctk.CTk):
             threading.Thread(target=self.download_sale_file, args=()),
             threading.Thread(target=self.download_fba_inventory, args=()),
             threading.Thread(target=self.download_dictionary, args=()),
-            threading.Thread(target=self.download_price_list, args=()),
         ]
 
         for thread in threads:
@@ -221,10 +219,9 @@ class App(ctk.CTk):
         event_file = self.result_files.get("event_file")
         fba_inventory = self.result_files.get("fba_inventory")
         dictionary = self.result_files.get("dictionary")
-        price_list = self.result_files.get("price_list")
 
         price_check = self.merge_files(
-            dictionary, price_list, fba_inventory, sale_file, event_file
+            dictionary, fba_inventory, sale_file, event_file
         )
         price_check_refined = self.process_file(price_check)
         _ = self.export_to_excel(price_check_refined)
